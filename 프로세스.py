86414929051a6aa24c4d26664f3cfb513f0ea237
Copyerrr <ssptotquf1@naver.com>
2025-08-20 17:47:20 +0900

    프로세스.py

---
+++ 프로세스.py
@@ -0,0 +1,22 @@
+def solution(priorities, location):
+    answer = 0
+    a = sorted(priorities, reverse=True)
+
+    current = 0
+    cnt =1
+    for i in a:
+        
+        for j in priorities[current:] + priorities[:current-1]:
+            if(current >= len(priorities)):
+                current = 0
+            if(i==j):
+                if(current == location):
+                    return cnt
+                current+=1
+                break
+            current+=1
+            
+        cnt += 1
+
+print(solution([2, 1, 3, 2]	, 2))
+print(solution([1, 1, 9, 1, 1, 1]	, 0))
