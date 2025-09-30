1a4292e8b0e7eafa3bb2566bcccef4624db7e55b
Copyerrr <ssptotquf1@naver.com>
2025-09-30 18:37:20 +0900

    네트워크..py

---
+++ 네트워크.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+def bfs(start, graph, visited):
+
+    q = deque([start])
+    visited[start] = True
+
+    while(q):
+        a = q.popleft()
+        for b in range(len(graph)):
+            if(graph[a][b] == 1 and visited[b] != True):
+                visited[b] == True
+                q.append(b)
+
+def solution(n, computers):
+    answer = 0
+
+    visited = [False] * n
+
+    for i in range(n):
+        if(visited[i] != True):
+            bfs(i,computers,visited)
+            answer+=1
+
+    return answer
