505b6fd897fca45acb55506d4551d9c37d2cc7fc
Copyerrr <ssptotquf1@naver.com>
2025-10-06 20:04:06 +0900

    [1차] 프렌즈4블록.py

---
+++ [1차] 프렌즈4블록.py
@@ -0,0 +1,12 @@
+def solution(m, n, board):
+    answer = 0
+
+    board = [list(row) for row in board]
+
+    return board
+
+
+    return answer
+
+
+print(solution(4	,5	,["CCBDE", "AAADE", "AAABF", "CCBBF"]))
